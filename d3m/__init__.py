21b77cebc90c77185541e943208e96a908c5b4fd
lhenry15 <khlai037@gmail.com>
2021-11-25 08:07:59 -0600

    modified to support py37+

--- d3m/__init__.py
+++ d3m/__init__.py
@@ -1,6 +1,6 @@
 # Use date-based versioning, year.month.date, without leading zeros
 # For the development branch, use *the day after* the last release with a `.dev0` suffix, for example 2019.2.13.dev0
-__version__ = '2020.11.4.dev0'
+__version__ = '2021.11.24'
 
 __description__ = 'Common code for D3M project'
 
